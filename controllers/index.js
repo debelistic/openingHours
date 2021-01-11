0280ea117c69b553e53af614d379b3311773ac9c
Victor Awotidebe <victorawotidebe@gmail.com>
2021-01-11 13:15:27 +0100

    finish

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
-// const mockData = require('../mockDB/hours.json')
 const { format } = require('../helpers')
 
 exports.HoursController = async (req, res) => {
   try {
-    const data = req.body.hours
-    const finData = format(data)
+    const { hours } = req.body
+    if (!hours) return res.status(400).send({ status: 'error', statusCode: 400, message: 'enter opening hours' })
+    const finData = format(hours)
 
     return res.status(200).send({
       status: 'success', statusCode: 200, message: 'success', data: finData,
